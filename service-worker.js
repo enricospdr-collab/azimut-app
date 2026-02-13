13a0add995d507744814b5990a637fd4d3968db0
enricospdr-collab <enricospdr@gmail.com>
2026-02-13 11:06:00 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,26 @@
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('azimut-app').then(function(cache) {
-      return cache.addAll(['index.html']);
+const CACHE_NAME = 'azimut-app-v3';
+const urlsToCache = ['index.html'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => { if(key !== CACHE_NAME) return caches.delete(key); })
+      );
     })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
